bf177906f34c303939628f4866b2cfd3cf3f3c19
larnaa <larnaa@localhost.localdomain>
2019-10-26 16:54:47 +0300

    Added method test_basket_button

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_basket_button(browser):
+    browser.get(link)
+    time.sleep(5)
+    assert browser.find_element_by_css_selector('.btn-add-to-basket')
