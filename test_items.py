dcb90d70920cd625da6277a2ea48e766b8d2971c
larnaa <larnaa@localhost.localdomain>
2019-10-28 16:37:19 +0300

    Added message error

--- test_items.py
+++ test_items.py
@@ -6,4 +6,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_basket_button(browser):
     browser.get(link)
     time.sleep(5)
-    assert browser.find_element_by_css_selector('.btn-add-to-basket')
+    try:
+        browser.find_element_by_css_selector('.btn-add-to-basket')
+    except:
+        assert False, "not button 'basket' here"
