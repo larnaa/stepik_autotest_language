14049051295fb074d7e616e562174d83c494896b
larnaa <larnaa@localhost.localdomain>
2019-10-28 15:40:23 +0300

    Removed explanation

--- test_items.py
+++ test_items.py
@@ -1,8 +1,5 @@
 import time
-'''
-По умолчанию установлен firefox, так что лучше использовать для запуска команду:
-pytest --browser_name=chrome --language=fr test_items.py
-'''
+
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
